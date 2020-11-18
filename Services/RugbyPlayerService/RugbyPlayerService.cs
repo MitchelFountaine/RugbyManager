52b105a1f0bec8d404fcd631190072f9495ff2e9
Mitchel Fountaine <mitchel.fountaine@gmail.com>
2020-11-18 16:23:40 +0200

    Add project files.

---
+++ Services/RugbyPlayerService/RugbyPlayerService.cs
@@ -0,0 +1,46 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.RugbyPlayerService
+{
+    public class RugbyPlayerService : IRugbyPlayerService
+    {
+        public List<RugbyPlayer> availableRugbyPlayers = new List<RugbyPlayer>();
+        public RugbyPlayerService()
+        {
+            CreateAvailablePlayers();
+        }
+        public async Task<List<RugbyPlayer>> AddRugbyPlayer(RugbyPlayer newPlayer)
+        {
+            availableRugbyPlayers.Add(newPlayer);
+            return availableRugbyPlayers;
+        }
+
+        public async Task<List<RugbyPlayer>> GetAllAvailable()
+        {
+            return availableRugbyPlayers;
+        }
+
+        public async Task<RugbyPlayer> GetById(int id)
+        {
+            return availableRugbyPlayers.FirstOrDefault(predicate => predicate.Id == id);
+        }
+
+        private void CreateAvailablePlayers()
+        {
+            for (int i = 1; i < 21; i++)
+            {
+                availableRugbyPlayers.Add(new RugbyPlayer
+                {
+                    Age = 10 + i,
+                    Id = i
+                });
+            }
+        }
+
+    }
+}
---
+++ WebAPI/Controllers/RugbyPlayerController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Models;
+using Services.RugbyPlayerService;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RugbyPlayerController : ControllerBase
+    {
+        private readonly IRugbyPlayerService _rugbyPlayerService;
+        public RugbyPlayerController(IRugbyPlayerService rugbyPlayerService)
+        {
+            _rugbyPlayerService = rugbyPlayerService;
+        }
+        [HttpGet("GetAllAvailable")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _rugbyPlayerService.GetAllAvailable());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult>GetSingle(int id)
+        {
+            return Ok(await _rugbyPlayerService.GetById(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRugbyPlayer(RugbyPlayer newPlayer)
+        {
+            return Ok(await _rugbyPlayerService.AddRugbyPlayer(newPlayer));
+        }
+    }
+}
